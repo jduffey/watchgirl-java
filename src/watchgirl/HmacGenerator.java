c83ce305b48f4c7fafdd47a092574d2221246b14
Jed Duffey <jedduffey@gmail.com>
2020-07-25 16:49:02 -0400

    minor formatting HmacGenerator

--- src/watchgirl/HmacGenerator.java
+++ src/watchgirl/HmacGenerator.java
@@ -9,18 +9,15 @@ import java.nio.charset.StandardCharsets;
 
 public class HmacGenerator {
 
-    /*
-    From https://stackoverflow.com/questions/7124735/hmac-sha256-algorithm-for-signature-calculation
-     */
+    // From https://stackoverflow.com/questions/7124735/hmac-sha256-algorithm-for-signature-calculation
+    public String generateHmac(String message, String key) throws Exception {
+        String algorithm = "HmacSHA256";
+        Charset charset = StandardCharsets.UTF_8;
 
-       public String generateHmac(String message, String key) throws Exception {
-           String algorithm = "HmacSHA256";
-           Charset charset = StandardCharsets.UTF_8;
+        Mac sha256Hmac = Mac.getInstance(algorithm);
+        SecretKeySpec secret_key = new SecretKeySpec(key.getBytes(charset), algorithm);
+        sha256Hmac.init(secret_key);
 
-           Mac sha256Hmac = Mac.getInstance(algorithm);
-           SecretKeySpec secret_key = new SecretKeySpec(key.getBytes(charset), algorithm);
-           sha256Hmac.init(secret_key);
-
-           return Hex.encodeHexString(sha256Hmac.doFinal(message.getBytes(charset)));
-       }
+        return Hex.encodeHexString(sha256Hmac.doFinal(message.getBytes(charset)));
+    }
 }
