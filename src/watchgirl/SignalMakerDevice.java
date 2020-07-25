10179b476c56bfa4b0c632ac36d7703173be12db
Jed Duffey <jedduffey@gmail.com>
2020-07-25 16:51:22 -0400

    refactor SignalMakerDevice (now takes secret and not SecretKeeper)

--- src/watchgirl/SignalMakerDevice.java
+++ src/watchgirl/SignalMakerDevice.java
@@ -1,47 +1,43 @@
-//package watchgirl;
-//
-//public class SignalMaker {
-//
-//    private final TimeKeeper timeKeeper;
-//    private final SecretKeeper secretKeeper;
-//    private final CustomHasher customHasher;
-//
-//    public SignalMaker(TimeKeeper timeKeeper, SecretKeeper secretKeeper, CustomHasher customHasher) {
-//        this.timeKeeper = timeKeeper;
-//        this.secretKeeper = secretKeeper;
-//        this.customHasher = customHasher;
-//    }
-//
-//    public SignalOutput generateSignal() {
-//        String digest = getDigest();
-//
-//        int lastDigitMod4 = getLastDigitMod4(digest);
-//
-//        SignalOutput generatedSignal = switch (lastDigitMod4) {
-//            case 0 -> SignalOutput.RED;
-//            case 1 -> SignalOutput.GREEN;
-//            case 2 -> SignalOutput.BLUE;
-//            case 3 -> SignalOutput.WHITE;
-//            default -> SignalOutput.SHOULD_NEVER_SEE_ME;
-//        };
-//
-//        System.out.println("[Generated Signal] " + generatedSignal);
-//        return generatedSignal;
-//    }
-//
-//    private String getDigest() {
-//        String currentTime = timeKeeper.getCurrentUnixTime();
-//        String secret = secretKeeper.getSecret(cameraID);
-//
-//        return customHasher.getDigest(currentTime, secret);
-//    }
-//
-//    private int getLastDigitMod4(String digest) {
-//        int lastIndexOfSha256Digest = 63;
-//        int hexRadix = 16;
-//        String lastDigit = digest.substring(lastIndexOfSha256Digest);
-//        int lastDigitAsInt = Integer.parseInt(lastDigit, hexRadix);
-//
-//        return lastDigitAsInt % 4;
-//    }
-//}
+package watchgirl;
+
+public class SignalMakerDevice {
+
+    private final TimeKeeper timeKeeper;
+    private final String secret;
+    private final HmacGenerator hmacGenerator;
+
+    public SignalMakerDevice(TimeKeeper timeKeeper, String secret, HmacGenerator hmacGenerator) {
+        this.timeKeeper = timeKeeper;
+        this.secret = secret;
+        this.hmacGenerator = hmacGenerator;
+    }
+
+    public SignalOutput generateSignal() throws Exception {
+        String digest = getDigest();
+
+        int lastDigitMod4 = getLastDigitMod4(digest);
+
+        return switch (lastDigitMod4) {
+            case 0 -> SignalOutput.RED;
+            case 1 -> SignalOutput.GREEN;
+            case 2 -> SignalOutput.BLUE;
+            case 3 -> SignalOutput.WHITE;
+            default -> SignalOutput.SHOULD_NEVER_SEE_ME;
+        };
+    }
+
+    private String getDigest() throws Exception {
+        String currentTime = timeKeeper.getCurrentUnixTime();
+
+        return hmacGenerator.generateHmac(currentTime, secret);
+    }
+
+    private int getLastDigitMod4(String digest) {
+        int lastIndexOfSha256Digest = 63;
+        int hexRadix = 16;
+        String lastDigit = digest.substring(lastIndexOfSha256Digest);
+        int lastDigitAsInt = Integer.parseInt(lastDigit, hexRadix);
+
+        return lastDigitAsInt % 4;
+    }
+}
