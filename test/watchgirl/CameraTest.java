2c74e419d8a115ab248c7aab568f720dc8b9cdc5
Jed Duffey <jedduffey@gmail.com>
2020-07-25 23:11:39 -0400

    minor cleanup CameraTest

--- test/watchgirl/CameraTest.java
+++ test/watchgirl/CameraTest.java
@@ -11,56 +11,42 @@ import static org.mockito.Mockito.*;
 
 public class CameraTest {
 
-    private static final String TIME_0 = "TIME_0";
-    private static final String TIME_1 = "TIME_1";
+    private static final String TIME = "TIME_0";
+    private final TimeKeeper timeKeeper = mock(TimeKeeper.class);
     private Camera camera;
     private UUID cameraId;
 
     @BeforeEach
     void setup() {
         cameraId = UUID.randomUUID();
-        TimeKeeper timeKeeper = mock(TimeKeeper.class);
-        when(timeKeeper.getCurrentUnixTime()).thenReturn(TIME_0, TIME_1);
         camera = new Camera(cameraId, timeKeeper);
+        when(timeKeeper.getCurrentUnixTime()).thenReturn(TIME);
     }
 
     @Test
     void takePhoto_storesTimeAndReceivedSignal() {
         camera.takePhoto(SignalOutput.RED);
-        camera.takePhoto(SignalOutput.GREEN);
 
         List<Photograph> storedPhotos = camera.getStoredPhotos();
 
-        Assertions.assertEquals(
-                SignalOutput.RED,
-                getSignalOfPhoto(storedPhotos, TIME_0));
-
-        Assertions.assertEquals(
-                SignalOutput.GREEN,
-                getSignalOfPhoto(storedPhotos, TIME_1));
+        Assertions.assertEquals(SignalOutput.RED, getSignalOfPhoto(storedPhotos));
     }
 
     @Test
     void takePhoto_storesCameraIdWithPhoto() {
-        TimeKeeper timeKeeper = mock(TimeKeeper.class);
-        when(timeKeeper.getCurrentUnixTime()).thenReturn(TIME_0, TIME_1);
-        Camera camera = new Camera(cameraId, timeKeeper);
-
         camera.takePhoto(SignalOutput.RED);
 
         List<Photograph> storedPhotos = camera.getStoredPhotos();
 
         Photograph photo = storedPhotos.stream().findFirst().orElse(null);
 
-        Assertions.assertEquals(
-                cameraId,
-                photo.getCameraId()
-        );
+        assert photo != null;
+        Assertions.assertEquals(cameraId, photo.getCameraId());
     }
 
-    private SignalOutput getSignalOfPhoto(List<Photograph> storedPhotos, String time) {
+    private SignalOutput getSignalOfPhoto(List<Photograph> storedPhotos) {
         Photograph photograph = storedPhotos.stream()
-                .filter(photo -> photo.getTime().equals(time))
+                .filter(p -> p.getTime().equals(TIME))
                 .findFirst()
                 .orElse(null);
 
